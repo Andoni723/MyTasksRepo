132dd91923a010f0ac8dc956550350cead28961b
aanibarr <aanibarr@host.docker.internal>
2020-12-22 20:13:15 +0100

    sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-/*INSERT INTO TASKS (DESCRIPTION, STATUS) VALUES ('Tarea numero uno','Completada');
+INSERT INTO TASKS (DESCRIPTION, STATUS) VALUES ('Tarea numero uno','Completada');
 INSERT INTO TASKS (DESCRIPTION, STATUS) VALUES ('Tarea numero dos','Pendiente');
-INSERT INTO TASKS (DESCRIPTION, STATUS) VALUES ('Tarea numero tres','En proceso');*/
+INSERT INTO TASKS (DESCRIPTION, STATUS) VALUES ('Tarea numero tres','En proceso');
