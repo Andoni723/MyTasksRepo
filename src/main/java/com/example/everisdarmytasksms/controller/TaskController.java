dd5cb9b31667570fd92db5f156a4dcb178d508b4
aanibarr <aanibarr@host.docker.internal>
2020-12-22 17:14:49 +0100

    CrossOrigin

--- src/main/java/com/example/everisdarmytasksms/controller/TaskController.java
+++ src/main/java/com/example/everisdarmytasksms/controller/TaskController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.everisdarmytasksms.Entity.tasks;
 import com.example.everisdarmytasksms.Service.TaskService;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class TaskController {
 
